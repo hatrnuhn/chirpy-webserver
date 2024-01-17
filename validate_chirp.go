3a5039016f185f9529de3748a34c31598ae70611
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-01-17 10:38:08 +0700

    add profanity to validate_chirp

--- validate_chirp.go
+++ validate_chirp.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 )
 
 func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
@@ -14,39 +15,51 @@ func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	type responseBody struct {
-		Valid string `json:"valid"`
+		Msg string `json:"cleaned_body"`
 	}
 
 	dat, err := io.ReadAll(r.Body)
 
 	if err != nil {
-
 		respondWithError(w, 500, "couldn't read request")
-
 		return
-
 	}
 
 	req := requestBody{}
 
 	err = json.Unmarshal(dat, &req)
-
 	if err != nil {
-
 		respondWithError(w, 500, "couldn't unmarshal request")
-
 		return
-
 	}
 
 	if len(req.Body) > 140 {
 		respondWithError(w, 400, "Chirp is too long!")
-
 		return
 	}
 
+	msg := strings.Split(req.Body, " ")
+
+	profaneWords := []string{
+		"kerfuffle",
+		"sharbert",
+		"fornax",
+	}
+
+	for i, word := range msg {
+		w := strings.ToLower(word)
+		for _, filWord := range profaneWords {
+			if w == filWord {
+				msg[i] = "****"
+				break
+			}
+		}
+	}
+
+	respMsg := strings.Join(msg, " ")
+
 	respondWithJSON(w, 200, responseBody{
-		Valid: "true",
+		Msg: respMsg,
 	})
 
 }
